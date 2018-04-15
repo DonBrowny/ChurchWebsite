840753b0cff86c2375f0486c6302d525802b9b0d
HellCat <kishore13ask@gmail.com>
2018-04-15 22:17:44 +0530

    initial push

---
+++ V1.0/js/custom.js
@@ -0,0 +1,10 @@
+$(function() {
+    // On Scroll
+	$(window).on('scroll', function() {
+		var wScroll = $(this).scrollTop();
+
+		// Fixed nav
+		wScroll > 1 ? $('#nav').addClass('fixed-nav') : $('#nav').removeClass('fixed-nav');
+
+	});
+});
