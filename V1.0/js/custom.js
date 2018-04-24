b7ce203adbf746ecc04fd3445e98710db4ae96b7
HellCat <kishore13ask@gmail.com>
2018-04-24 06:51:32 +0530

    fixes & features
    Added dynamc latet video on message screen
    optimized request form

--- V1.0/js/custom.js
+++ V1.0/js/custom.js
@@ -1,4 +1,11 @@
-$(function() {
+$(function () {
+	let videoArray = new Array();
+	const getVideoId = (array) => {return array[0] };
+	const getVideoTitle = (array) => {return array[1] };
+	const getVideoImg = (array) => {return array[2] };
+	const embedURL = (videoId) => { return "https://www.youtube.com/embed/" + videoId};
+	const viewURL = (videoId) => { return "https://www.youtube.com/watch?v=" + videoId};
+	
 	$('body').scrollspy({
 		target: '.navbar',
 		offset: $(window).height() / 2
@@ -23,9 +30,9 @@ $(function() {
 	});
 	$('.nav-link').on('click', function () {
 		$('.animated-icon').toggleClass('open');
-	});	
+	});
 
-    // On Scroll
+	// On Scroll
 	// $(window).on('scroll', function() {
 	// 	var wScroll = $(this).scrollTop();
 
@@ -33,4 +40,39 @@ $(function() {
 	// 	wScroll > 1 ? $('#nav').addClass('fixed-nav') : $('#nav').removeClass('fixed-nav');
 
 	// });
+	$.ajax({
+		type: "GET",
+		url: "https://www.googleapis.com/youtube/v3/search",
+		datatype: "json",
+		data: {
+			part: 'snippet',
+			channelId: 'UC8YADFpAa4BFBSYUmkYwWvA',
+			maxResults: 10,
+			order: 'date',
+			type: 'video',
+			fields:'items(id/videoId,snippet(thumbnails/high/url,title))',
+			key: 'AIzaSyBlq5YOCz42CLmQIDCfny0xrcOyaDCFQgA'
+		},
+		success: function (data) {
+			var list = data.items;
+			for (let index = 0; index < list.length; index++) {
+				const element = list[index];
+				let tempArray = new Array(element.id.videoId,element.snippet.title,element.snippet.thumbnails.high.url);				
+				videoArray.push(tempArray);
+			}
+			setMainVideoSrc(embedURL( getVideoId(videoArray[0])));
+			setMainVideoTitle( getVideoTitle(videoArray[0]));
+		}
+	});
 });
+
+function setMainVideoSrc(url){
+	let videoDiv = document.getElementById('latest-video');
+	videoDiv.src = url;
+}
+
+function setMainVideoTitle(title){
+	let titleDiv = document.getElementById('videoTitle');
+	console.log(titleDiv);
+	titleDiv.innerText = title;
+}
